c06de74cf7ad48919664e392b7f1a3ce88395c1c
dajung <ekwjd990913@gmail.com>
2025-04-25 21:36:09 +0900

    [feat] 경주 시작부, 결과부 구현

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,6 +1,7 @@
 fun main() {
     var scoreMap: MutableMap<String, Int>
-    var tryCount: Int?
+    var cars: Set<String>
+    var tryCount: Int
 
     while(true) {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
@@ -9,7 +10,9 @@ fun main() {
             if(input1.isNullOrBlank()) {
                 throw IllegalArgumentException("[ERROR] 입력이 없습니다.")
             }
-            val cars = input1.split(",").map { it.trim() }
+            cars = input1.split(",")
+                .map { it.trim() }
+                .toSet()
             val invalid = cars.firstOrNull { it.length > 5 }
             if(invalid != null) {
                 throw IllegalArgumentException("[ERROR] 5자리를 초과한 이름이 있습니다: $invalid")
@@ -28,12 +31,29 @@ fun main() {
                 throw IllegalArgumentException("[ERROR] 입력이 없습니다.")
             }
             tryCount = input2.toIntOrNull()
-            if (tryCount == null) {
-                throw IllegalArgumentException("[ERROR] 횟수는 정수로 입력하세요.")
-            }
+                ?: throw IllegalArgumentException("[ERROR] 횟수는 정수로만 입력하세요.")
             break
         } catch (e: IllegalArgumentException) {
             println(e.message)
         }
     }
+
+    for(game in 1..tryCount) {
+        for(car in cars) {
+            val random = (0..9).random()
+            var currentScore = scoreMap[car]!!
+            if(random >= 4) {
+                currentScore++
+                scoreMap[car] = currentScore
+            }
+            println("$car: ${ "-".repeat(currentScore)}")
+        }
+        println("")
+    }
+
+    val maxScore = scoreMap.values.maxOrNull()
+    val winners = scoreMap.filterValues { it == maxScore }
+        .keys
+        .joinToString(", ")
+    print("최종 우승자 : $winners")
 }
