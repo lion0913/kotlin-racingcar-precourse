24b8c1ce67a22db9eb51b8be1f15a469b462045e
dajung <ekwjd990913@gmail.com>
2025-04-26 12:17:05 +0900

    [refactor] View 역할에 맞게 리팩토링

--- src/main/kotlin/racing/controller/RaceController.kt
+++ src/main/kotlin/racing/controller/RaceController.kt
@@ -7,8 +7,8 @@ import racing.view.WebView
 
 class RaceController {
     fun run() {
-        val carNames = WebView().inputCarNames()
-        val tryCount = WebView().inputTryCount()
+        val carNames = validateCarNames()
+        val tryCount = validateTryCount()
         val cars = carNames.map { Car(it) }
 
         val race = Race(cars)
@@ -19,5 +19,30 @@ class RaceController {
         }
         val winners = race.getWinners()
         WebView().printResults(winners)
+    }
+
+    fun validateCarNames(): List<String> {
+        while(true) {
+            try {
+                val cars = WebView().inputCarNames()
+                val invalid = cars.firstOrNull { it.length > 5 }
+                if (invalid != null) {
+                    throw IllegalArgumentException("[ERROR] 5자리를 초과한 이름이 있습니다: $invalid")
+                }
+                return cars.toSet().toList()
+            } catch(e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    fun validateTryCount(): Int {
+        while(true) {
+            try {
+                return WebView().inputTryCount()
+            } catch(e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 }
